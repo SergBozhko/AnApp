3d69a5a0eae32b214d190120d22bb69fa7dd0aca
Admin <admin@Mac-mini-FrontEnd.local>
2015-10-30 10:23:27 +0300

    Final ver

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,48 @@
 $(function() {
 	console.log('DOM ready...');
 
-	$('.make-slider').slick({
+	new WOW().init();
+
+	// Mobile menu
+	var $mobileMenu = $('.mobile-nav-menu'),
+		$mobileOverlay = $('.mobile-overlay'),
+		$mobileButton = $('.mobile-menu-btn, .mobile-overlay'),
+		mActive = 'mobile-nav-menu-active',
+		oActive = 'mobile-overlay-active',
+		oHidden = 'o-hidden',
+		$body = $('body');
+	
+	$mobileButton.click(function(e) {
+		e.preventDefault();
+
+		$mobileMenu.toggleClass(mActive);
+		$mobileOverlay.toggleClass(oActive);
+		$body.toggleClass(oHidden);
+	});
+
+	// Sliders
+	$('.slider-desktop').slick({
 	  infinite: true,
 	  slidesToShow: 3,
 	  slidesToScroll: 3,
 	  autoplay: true,
   	  autoplaySpeed: 6000
+	});
+	$('.slider-mobile').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+  	  autoplaySpeed: 6000
+	});
+	$('.phone-slider-desktop').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 3
+	});
+	$('.phone-slider-mobile').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1
 	});
 });
